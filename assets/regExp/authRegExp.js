d97b726cfd5cfe71f41d49a7dca581c3b546163d
Sergey Korobka <nejdancs@gmail.com>
2022-12-03 23:05:09 +0200

    fix  regex

--- assets/regExp/authRegExp.js
+++ assets/regExp/authRegExp.js
@@ -1,6 +1,5 @@
 const emailRegExp = /^\w+([.-]?\w+){2}@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-const nameRegExp =
-    /^[a-zA-Zа-яёА-ЯЁА-ЩЬЮЯҐЄІЇа-щьюяґєії0-9][a-zA-Zа-яёА-ЯЁА-ЩЬЮЯҐЄІЇа-щьюяґєії 0-9]+$/;
+const nameRegExp = /^[a-zA-Zа-яёА-ЯЁА-ЩЬЮЯҐЄІЇа-щьюяґєії0-9]+$/;
 const passwordRegExp = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*]{6,12}$/;
 
 module.exports = { emailRegExp, nameRegExp, passwordRegExp };
