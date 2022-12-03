451208a897758ab9d646adf1b60c174847ca9d38
Sergey Korobka <nejdancs@gmail.com>
2022-12-03 12:14:35 +0200

    fix

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const authRouter = require("./routes/api/auth");
 const usersRouter = require("./routes/api/users");
-const swaggerRouter = require("./routes/api/swagger");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-const pathPublic = path.join(__dirname, "public");
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs/public", express.static(pathPublic));
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
-app.use("/api-docs", swaggerRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
     res.status(404).json({ status: "error", code: 404, message: "Not found" });
