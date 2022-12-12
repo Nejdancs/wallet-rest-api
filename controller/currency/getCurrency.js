f0c7a9cd6f801c1b0531bf2631464c7b54608e15
Sergey Korobka <nejdancs@gmail.com>
2022-12-12 21:10:34 +0000

    add m currency memoData

--- controller/currency/getCurrency.js
+++ controller/currency/getCurrency.js
@@ -1,15 +1,48 @@
 const { fetchCurrency } = require("../../services");
+const { GatewayTimeout } = require("http-errors");
 
-const getCurrency = async (_, res) => {
-    const data = await fetchCurrency();
+let lastUpdateDate = null;
+let MEMO_DATA = null;
+let attempts = 0;
 
-    res.status(200).json({
-        status: "success",
-        code: 200,
-        data: {
-            currency: data,
-        },
-    });
+const getCurrency = async (_, res, next) => {
+    const timePassed = (lastUpdateDate) => {
+        const timeDifference = Date.now() - lastUpdateDate;
+        const result = timeDifference >= 60000;
+        return result;
+    };
+
+    const fetch = async () => {
+        try {
+            if (!MEMO_DATA || timePassed(lastUpdateDate)) {
+                const data = await fetchCurrency();
+
+                MEMO_DATA = data;
+                lastUpdateDate = Date.now();
+            }
+
+            res.status(200).json({
+                status: "success",
+                code: 200,
+                data: {
+                    currency: MEMO_DATA,
+                },
+            });
+        } catch (error) {
+            attempts += 1;
+            console.log(attempts);
+
+            if (attempts > 4) {
+                attempts = 0;
+                console.log(error);
+                next(GatewayTimeout("Timeout"));
+            } else {
+                setTimeout(() => fetch(), 1000);
+            }
+        }
+    };
+
+    fetch();
 };
 
 module.exports = getCurrency;
